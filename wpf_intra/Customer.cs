0cb344cb69a135440e429dd9b33faf92e354945c
Jérémy Bernard <jere.bern@hotmail.com>
2020-02-24 13:38:55 -0500

    COMIT

---
+++ wpf_intra/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel; //INotifyPropertyChange
+
+namespace wpf_intra
+{
+    class Customer : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Adresse { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string Info;
+    }
+}
